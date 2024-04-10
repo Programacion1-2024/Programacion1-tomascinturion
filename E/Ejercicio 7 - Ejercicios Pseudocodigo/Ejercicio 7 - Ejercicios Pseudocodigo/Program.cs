2e28caf7c54720ed32ce4107b3c468589c57a8a0
Tomascinturion <tomascinturionices@gmail.com>
2024-04-10 20:02:36 -0300

    Ejercicio 7

---
+++ E/Ejercicio 7 - Ejercicios Pseudocodigo/Ejercicio 7 - Ejercicios Pseudocodigo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+Console.Write("Ingrese un numero: ");
+float num1, num2, mayor;
+if(!float.TryParse(Console.ReadLine(), out num1))
+{
+    Console.WriteLine("Error, ingrese un numero");
+}
+else
+{
+    Console.Write("Ingrese el segundo numero: ");
+    if(!float.TryParse(Console.ReadLine(),out num2))
+    {
+        Console.WriteLine("Error, ingrese un numero");
+    }
+    else
+    {
+        if(num1 > num2)
+        {
+            mayor = num1;
+            Console.WriteLine("El mayor de los dos numeros es el: " + mayor);
+        }
+        else
+        {
+            mayor = num2;
+            Console.WriteLine("El mayor de los dos numeros es el: " + mayor);
+        }
+    }
+}
+
