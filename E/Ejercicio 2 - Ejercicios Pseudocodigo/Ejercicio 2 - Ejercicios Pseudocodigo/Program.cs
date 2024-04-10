5218bc04fc8b099d286d613dd71d01778d422023
Tomascinturion <tomascinturionices@gmail.com>
2024-04-10 19:37:49 -0300

    Ejecicios 1-4

---
+++ E/Ejercicio 2 - Ejercicios Pseudocodigo/Ejercicio 2 - Ejercicios Pseudocodigo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+
+Console.Write("Ingrese el numero: ");
+int numero;
+if (!Int32.TryParse(Console.ReadLine(), out numero))
+{
+    Console.WriteLine("Error al ingresar el número.");
+}
+else
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("Es Par");
+    }
+    else
+    {
+        Console.WriteLine("Es Impar");
+    }
+}
