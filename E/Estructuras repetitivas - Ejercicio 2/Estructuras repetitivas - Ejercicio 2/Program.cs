25f2e83f4379c3894e2d14e438dc5df96e0cf19f
Tomascinturion <tomascinturionices@gmail.com>
2024-04-16 17:31:41 -0300

    Ej 2

---
+++ E/Estructuras repetitivas - Ejercicio 2/Estructuras repetitivas - Ejercicio 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+Console.WriteLine("Ingrese los numeros: ");
+
+float promedio = 0;
+for (int i = 0; i <= 7; i++)
+{
+    Console.Write($"Numero {i}: ");
+    if (!float.TryParse(Console.ReadLine(), out float suma))
+    {
+        Console.WriteLine("Error, ingrese un numero valido");
+        i--;
+    }
+    else
+    {
+        promedio += suma;
+    }
+}
+float resutado = promedio / 8;
+Console.WriteLine($"El promedio es de: {resutado}");
