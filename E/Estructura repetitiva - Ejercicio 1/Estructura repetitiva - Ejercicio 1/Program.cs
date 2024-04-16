53bb329690fa35878b65f5090cd448f61968a617
Tomascinturion <tomascinturionices@gmail.com>
2024-04-16 17:15:39 -0300

    Ej 1

---
+++ E/Estructura repetitiva - Ejercicio 1/Estructura repetitiva - Ejercicio 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Timers;
+Console.Write("Ingrese 5 numeros: ");
+
+float suma = 0;
+for (int i = 0; i <= 4; i++)
+{
+    Console.Write($"valor {i}: ");
+    if(!float.TryParse(Console.ReadLine(), out float numero))
+    {
+        Console.WriteLine("Error, numero no valido");
+        i--;
+    }
+    else
+    {
+        suma += numero;
+    }
+}
+Console.WriteLine($"El valor de la suma es de: {suma}");
