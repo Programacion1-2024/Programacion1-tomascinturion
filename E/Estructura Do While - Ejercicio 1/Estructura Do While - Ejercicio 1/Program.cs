3354cbdf38d1d6765d7a33fb8bfa9d267f3ba0bc
Tomascinturion <tomascinturionices@gmail.com>
2024-04-23 18:36:23 -0300

    Ej 1 y 2 do while

---
+++ E/Estructura Do While - Ejercicio 1/Estructura Do While - Ejercicio 1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Para el ingreso a un sistema se necesita solicitar nombre de usuario y clave, repetir este ingreso hasta que el usuario ingrese la clave correcta: 667788.
+using System;
+int contra;
+
+do
+{
+    Console.Write("Usuario: ");
+    Console.ReadLine();
+    Console.Write("Contraseña: ");
+    if (!int.TryParse(Console.ReadLine(), out contra) && contra != 667788)
+    {
+        Console.WriteLine("Contraseña incorrecta");
+    }
+
+}
+while (contra != 667788);
+Console.WriteLine("Ingresando");
---
+++ E/Estructura DoWhile - Ejercicio 2/Estructura DoWhile - Ejercicio 2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Se le solicitará ingresar un número al usuario, se repetirá el pedido hasta que ingrese un positivo.
+float numero = 0;
+do
+{
+    Console.Write("Ingrese un numero: ");
+    if (!float.TryParse(Console.ReadLine(), out numero))
+    {
+        Console.WriteLine("Error, ingrese un numero valido");
+    }
+}
+while (numero < 0);
