e5e557f2c9a802ec352a1b1ab388a8185f17108f
Tomascinturion <tomascinturionices@gmail.com>
2024-05-07 17:48:35 -0300

    Ej Lista 1 y 2

---
+++ E/Ejercicios Lista - Ejercicio 1/Ejercicios Lista - Ejercicio 1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Ejercicio 3: Ingresar datos en una lista y mostrarlos
+//Consigna: El usuario debe ingresar 5 números enteros. El programa debe almacenar estos
+//números en una lista y luego mostrarlos en la consola.
+using System;
+  
+List<int> numeros = new List<int>();
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Ingrese un numero entero: ");
+    if(int.TryParse(Console.ReadLine(),out int num))
+    {
+        numeros.Add(num);
+    }
+    else
+    {
+        Console.WriteLine("Error, ingrese un numero valido.");
+        i--;
+    }
+}
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
---
+++ E/Ejercicios Lista - Ejercicio 2/Ejercicios Lista - Ejercicio 2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Ejercicio 4: Calcular promedio de notas utilizando una lista
+//Consigna: El usuario debe ingresar 5 notas. El programa debe calcular el promedio de estas
+//notas y mostrarlo en la consola. Utiliza una lista para almacenar las notas.
+using System;
+
+List<float> notas = new List<float>();
+float suma = 0;
+float promedio = 0;
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Ingrese una nota: ");
+    if(float.TryParse(Console.ReadLine(), out float not))
+    {
+        notas.Add(not);
+        suma += not;
+    }
+    else
+    {
+        Console.WriteLine("Error, ingrese un numero valido");
+        i--;
+    }
+}
+Console.WriteLine(promedio = suma / notas.Count());
