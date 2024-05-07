1654c7bdd9734d81779cb1777e8675d35afe5d19
Tomascinturion <tomascinturionices@gmail.com>
2024-05-07 17:21:05 -0300

    Ej arreglos 1 y 2

---
+++ E/Ejercicios Arreglos - Ejercicio 1/Ejercicios Arreglos - Ejercicio 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Ejercicio 1: Ingresar datos en un arreglo y mostrarlos
+//Consigna: El usuario debe ingresar 5 números enteros. El programa debe almacenar estos
+//números en un arreglo y luego mostrarlos en la consola.
+
+
+using System;
+
+int[] numeros = new int[5];
+int num = 0;
+for (int i = 0; i <= 4; i++)
+{
+    Console.Write("Ingrese un numero entero: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+    {
+        numeros[i] = num;
+    }
+    else
+    {
+        Console.WriteLine("Error, ingrese un numero valido");
+        i--;
+    }
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"{numeros[i]}, ");
+}
---
+++ E/Ejercicios Arreglos - Ejercicio 2/Ejercicios Arreglos - Ejercicio 2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Ejercicio 2: Calcular promedio de notas utilizando un arreglo
+//Consigna: El usuario debe ingresar 5 notas. El programa debe calcular el promedio de estas
+//notas y mostrarlo en la consola. Utiliza un arreglo para almacenar las notas.
+
+using System;
+
+float[] notas = new float[5];
+float not = 0;
+float promedio = 0;
+float suma = 0;
+
+for (int i = 0; i <= 4; i++)
+{
+    Console.Write("Ingrese una nota: ");
+    if (float.TryParse(Console.ReadLine().Replace(".",","), out not))
+    {
+        notas[i] = not;
+        suma += notas[i];
+    }
+    else
+    {
+        Console.WriteLine("Error, ingrese un numero valido");
+        i--;
+    }
+}
+Console.WriteLine(promedio = suma / notas.Length);
